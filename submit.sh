a1e332ed1417f5061f43f362d5b6db1fdca71cf2
ShumengJ <58873325+ShumengJ@users.noreply.github.com>
2025-04-26 23:52:38 -0400

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -8,5 +8,5 @@ module load StdEnv/2020 gcc/9.3.0 cuda/11.8 cudnn/8.6 opencv/4.5.5
 module load python/3.9.6
 source $HOME/ENV/bin/activate
 
-tensorboard --logdir=$HOME/scratch/Finch_Aug2024/Tensorflow11/seg_dis_slow/logs --host 0.0.0.0 --load_fast false &
-python $HOME/scratch/Finch_Aug2024/Tensorflow11/seg_dis_slow/train.py
+tensorboard --logdir=$HOME/scratch/OstrichR496/Tensorflow/SRSegGAN/logs --host 0.0.0.0 --load_fast false &
+python $HOME/scratch/OstrichR496/Tensorflow/SRSegGAN/train.py
